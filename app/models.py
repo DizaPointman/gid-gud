4fb68273f9a392c9eeea3ae97d4e1a703b5e0ea9
Diza Pointman <bifiroll86@googlemail.com>
2024-02-04 17:17:55 +0100

    setup user profile page and avatar

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db, login
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -14,6 +15,10 @@ class User(UserMixin, db.Model):
 
     todos: so.WriteOnlyMapped['ToDo'] = so.relationship(back_populates='author')
 
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
     
@@ -22,6 +27,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
     
 class ToDo(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
